6d132d877ed76932d6d505670ee89e5da690a5dd
Oscar <oscar.nordstrom2000@gmail.com>
2023-03-14 08:37:35 +0100

    Can execute current byte_code file also

--- main.cpp
+++ main.cpp
@@ -14,16 +14,16 @@ int main()
     
     
     std::string input;
-    std::cout<<"Choose what file to interpret:\n1: E.java\n2: D1.java\n3: D2.java\n4: D3.java\n5: C1.java\n6: C2.java\n7: B.java\n8: A.java\nPresse anyting else to exit\n";
+    std::cout<<"Choose what file to interpret:\n0: Current\n1: E.java\n2: D1.java\n3: D2.java\n4: D3.java\n5: C1.java\n6: C2.java\n7: B.java\n8: A.java\nPresse anyting else to exit\n";
     while(checkInput(input))
     {
       std::cout<<"\nOutput:\n";
       doFile(std::stoi(input));
-      std::cout<<"Press any key to continue";
+      std::cout<<"Press any Enter to continue";
       std::cin.ignore();
       std::cin.get();
       system("clear");
-      std::cout<<"Choose what file to interpret:\n1: E.java\n2: D1.java\n3: D2.java\n4: D3.java\n5: C1.java\n6: C2.java\n7: B.java\n8: A.java\nPresse anyting else to exit\n";
+      std::cout<<"Choose what file to interpret:\n0: Current\n1: E.java\n2: D1.java\n3: D2.java\n4: D3.java\n5: C1.java\n6: C2.java\n7: B.java\n8: A.java\nPresse anyting else to exit\n";
     }
 
     return 0;
@@ -33,7 +33,8 @@ bool checkInput(std::string& input)
 {
   
   std::cin>>input;
-  if(input == "1"
+  if(input == "0"
+  ||input == "1"
   ||input == "2"
   ||input == "3"
   ||input == "4"
@@ -52,6 +53,9 @@ void doFile(int num)
   std::string name;
   switch (num)
   {
+  case 0:
+    name = "byte_code.txt";
+    break;
   case 1:
     name = "test_bytecode/E.txt";
     break;
